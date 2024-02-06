11422370c20b329ee7afe80ced54aad2a2b56e3e
74568Sachin <sachinmpatil1238@gmail.com>
2024-02-06 13:06:02 +0530

    .net Repo

---
+++ DotNetFoodEase/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DotNetFoodEase.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
