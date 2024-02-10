660346a20e66ad139cc78269e14a039363e85aec
74568Sachin <sachinmpatil1238@gmail.com>
2024-02-11 01:55:49 +0530

    Validation add for login and customer Registration

--- react_foodease/src/App.js
+++ react_foodease/src/App.js
@@ -3,14 +3,25 @@ import Login from './login_form';
 import Customer from './Cust_Registration';
 import Restaurants from './Rest_Registration';
 import Delivery from './Delivery_Registration';
+import { Routes, Route, BrowserRouter } from 'react-router-dom';
 function App() {
   return (
     <div className='App'>
-      <Login/>
-      <Customer/>
-      <Restaurants/>
-      <Delivery/>
+      <BrowserRouter>
+      <Routes>
+          <Route path='/' element={<Login/>} />
+          {/* <Route path='/home' element={<Navbar/>} /> */}
+          <Route path='/login' element={<Login/>} />
+          <Route path='/customer_registration' element={<Customer/>} />
+          <Route path='/deliveryBoy_registration' element={<Delivery/>} />
+          <Route path='/rOwner_registration' element={<Restaurants/>}/>
+        </Routes>
+      </BrowserRouter>
+      
+    
     </div>
+
+    
   );
 }
 export default App; 
